bb2ff626fc9b234008231ff6555d42aa667fad29
Joachim Hill-Grannec <joachim@liffft.com>
2017-02-13 16:39:59 -0800

    First try to convert the ECS plugin to .5

--- main.go
+++ main.go
@@ -1,192 +1,101 @@
 package main
 
 import (
-	"fmt"
+	"github.com/codegangsta/cli"
+	"log"
 	"os"
-	"strconv"
-	"strings"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ecs"
-	"github.com/drone/drone-go/drone"
-	"github.com/drone/drone-go/plugin"
 )
 
-var (
-	buildCommit string
-)
+var version string
 
 func main() {
-	fmt.Printf("Drone AWS ECS Plugin built from %s\n", buildCommit)
-
-	workspace := drone.Workspace{}
-	repo := drone.Repo{}
-	build := drone.Build{}
-	vargs := Params{}
-
-	plugin.Param("workspace", &workspace)
-	plugin.Param("repo", &repo)
-	plugin.Param("build", &build)
-	plugin.Param("vargs", &vargs)
-	plugin.MustParse()
-
-	if len(vargs.AccessKey) == 0 {
-		fmt.Println("Please provide an access key")
-
-		os.Exit(1)
-		return
-	}
-
-	if len(vargs.SecretKey) == 0 {
-		fmt.Println("Please provide a secret key")
-
-		os.Exit(1)
-		return
-	}
-
-	if len(vargs.Region) == 0 {
-		fmt.Println("Please provide a region")
-
-		os.Exit(1)
-		return
-	}
-
-	if len(vargs.Family) == 0 {
-		fmt.Println("Please provide a task definition family name")
-
-		os.Exit(1)
-		return
-	}
-
-	if len(vargs.Image) == 0 {
-		fmt.Println("Please provide an image name")
-
-		os.Exit(1)
-		return
-	}
-
-	if len(vargs.Tag) == 0 {
-		vargs.Tag = "latest"
-	}
-
-	if len(vargs.Service) == 0 {
-		fmt.Println("Please provide a service name")
-
-		os.Exit(1)
-		return
-	}
-
-	if len(vargs.Cluster) == 0 {
-		fmt.Println("Cluster: default")
-	} else {
-		fmt.Printf("Cluster: %s\n", vargs.Cluster)
-	}
-
-	if vargs.Memory == 0 {
-		vargs.Memory = 128
-	}
-
-	svc := ecs.New(
-		session.New(&aws.Config{
-			Region:      aws.String(vargs.Region),
-			Credentials: credentials.NewStaticCredentials(vargs.AccessKey, vargs.SecretKey, ""),
-		}))
-
-	Image := vargs.Image + ":" + vargs.Tag
-
-	definition := ecs.ContainerDefinition{
-		Command: []*string{},
-
-		DnsSearchDomains:      []*string{},
-		DnsServers:            []*string{},
-		DockerLabels:          map[string]*string{},
-		DockerSecurityOptions: []*string{},
-		EntryPoint:            []*string{},
-		Environment:           []*ecs.KeyValuePair{},
-		Essential:             aws.Bool(true),
-		ExtraHosts:            []*ecs.HostEntry{},
-
-		Image:        aws.String(Image),
-		Links:        []*string{},
-		Memory:       aws.Int64(vargs.Memory),
-		MountPoints:  []*ecs.MountPoint{},
-		Name:         aws.String(vargs.Family + "-container"),
-		PortMappings: []*ecs.PortMapping{},
-
-		Ulimits: []*ecs.Ulimit{},
-		//User: aws.String("String"),
-		VolumesFrom: []*ecs.VolumeFrom{},
-		//WorkingDirectory: aws.String("String"),
-	}
-
-	// Port mappings
-	for _, portMapping := range vargs.PortMappings.Slice() {
-		cleanedPortMapping := strings.Trim(portMapping, " ")
-		parts := strings.SplitN(cleanedPortMapping, " ", 2)
-		hostPort, hostPortErr := strconv.ParseInt(parts[0], 10, 64)
-		if hostPortErr != nil {
-			fmt.Println(hostPortErr.Error())
-			os.Exit(1)
-			return
-		}
-		containerPort, containerPortError := strconv.ParseInt(parts[1], 10, 64)
-		if containerPortError != nil {
-			fmt.Println(containerPortError.Error())
-			os.Exit(1)
-			return
-		}
-
-		pair := ecs.PortMapping{
-			ContainerPort: aws.Int64(containerPort),
-			HostPort:      aws.Int64(hostPort),
-			Protocol:      aws.String("TransportProtocol"),
-		}
-
-		definition.PortMappings = append(definition.PortMappings, &pair)
-	}
-
-	// Environment variables
-	for _, envVar := range vargs.Environment.Slice() {
-		parts := strings.SplitN(envVar, "=", 2)
-		pair := ecs.KeyValuePair{
-			Name:  aws.String(strings.Trim(parts[0], " ")),
-			Value: aws.String(strings.Trim(parts[1], " ")),
-		}
-		definition.Environment = append(definition.Environment, &pair)
-	}
-	params := &ecs.RegisterTaskDefinitionInput{
-		ContainerDefinitions: []*ecs.ContainerDefinition{
-			&definition,
+	app := cli.NewApp()
+	app.Name = "rancher publish"
+	app.Usage = "rancher publish"
+	app.Action = run
+	app.Version = version
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "access-key",
+			Usage:  "AWS access key",
+			EnvVar: "PLUGIN_ACCESS_KEY, ECS_ACCESS_KEY",
 		},
-		Family:  aws.String(vargs.Family),
-		Volumes: []*ecs.Volume{},
+		cli.StringFlag{
+			Name:   "secret-key",
+			Usage:  "AWS secret key",
+			EnvVar: "PLUGIN_SECRET_KEY, ECS_SECRET_KEY",
+		},
+		cli.StringFlag{
+			Name:   "region",
+			Usage:  "aws region",
+			EnvVar: "PLUGIN_REGION",
+		},
+		cli.StringFlag{
+			Name:   "family",
+			Usage:  "ECS family",
+			EnvVar: "PLUGIN_FAMILY,",
+		},
+		cli.StringFlag{
+			Name:   "service",
+			Usage:  "Service to act on",
+			EnvVar: "PLUGIN_SERVICE",
+		},
+		cli.StringFlag{
+			Name:   "docker-image",
+			Usage:  "image to use",
+			EnvVar: "PLUGIN_DOCKER_IMAGE",
+		},
+		cli.StringFlag{
+			Name:   "tag",
+			Usage:  "AWS tag",
+			EnvVar: "PLUGIN_TAG",
+		},
+		cli.StringFlag{
+			Name:   "cluster",
+			Usage:  "AWS ECS cluster",
+			EnvVar: "PLUGIN_CLUSTER",
+		},
+		cli.StringSliceFlag{
+			Name:	"port-mappings",
+			Usage:	"ECS port maps",
+			EnvVar: "PLUGIN_PORT_MAPPINGS",
+		},
+		cli.StringSliceFlag{
+			Name:	"environment-variables",
+			Usage:	"ECS environment-variables",
+			EnvVar: "PLUGIN_ENVIRONMENT_VARIABLES",
+		},
+		cli.Int64Flag{
+			Name:   "memory",
+			Usage:  "Amount of memory to the conatiner defaults to 128",
+			Value:  128,
+			EnvVar: "PLUGIN_MEMORY",
+		},
+		cli.BoolTFlag{
+			Name:   "yaml-verified",
+			Usage:  "Ensure the yaml was signed",
+			EnvVar: "DRONE_YAML_VERIFIED",
+		},
 	}
-	resp, err := svc.RegisterTaskDefinition(params)
-
-	if err != nil {
-		fmt.Println(err.Error())
-
-		os.Exit(1)
-		return
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
+}
 
-	val := *(resp.TaskDefinition.TaskDefinitionArn)
-	sparams := &ecs.UpdateServiceInput{
-		Cluster:        aws.String(vargs.Cluster),
-		Service:        aws.String(vargs.Service),
-		TaskDefinition: aws.String(val),
+func run(c *cli.Context) error {
+	plugin := Plugin{
+		Key:            c.String("access-key"),
+		Secret:         c.String("secret-key"),
+		Region:         c.String("region"),
+		Family:         c.String("family"),
+		Service:        c.String("service"),
+		DockerImage:    c.String("docker-image"),
+		Tag:            c.String("tag"),
+		Cluster:        c.String("cluster"),
+		PortMappings:   c.StringSlice("port-mappings"),
+		Environment:    c.StringSlice("environment-variables"),
+		Memory:         c.Int64("memory"),
+		YamlVerified:   c.BoolT("yaml-verified"),
 	}
-	sresp, serr := svc.UpdateService(sparams)
-
-	if serr != nil {
-		fmt.Println(serr.Error())
-		os.Exit(1)
-		return
-	}
-
-	fmt.Println(sresp)
-
-	fmt.Println(resp)
+	return plugin.Exec()
 }
