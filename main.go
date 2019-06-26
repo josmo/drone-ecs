2f89e20fba9db056801dbdde9f583e676f806198
Anne van de Venis <annuh@users.noreply.github.com>
2019-06-26 12:45:45 +0200

    Update main.go

--- main.go
+++ main.go
@@ -185,6 +185,12 @@ func main() {
 			Value:  5,
 			EnvVar: "PLUGIN_HEALTHCHECK_TIMEOUT",
 		},
+		cli.StringFlag{
+			Name:   "service-network-assign-public-ip",
+			Usage:  "Assign public IP (ENABLED|DISABLED), defaults to DISABLED",
+			Value:  "DISABLED",
+			EnvVar: "PLUGIN_SERVICE_NETWORK_ASSIGN_PUBLIC_IP",
+		},
 		cli.StringSliceFlag{
 			Name:   "service-network-security-groups",
 			Usage:  "The security groups to associate with the service",
@@ -235,6 +241,7 @@ func run(c *cli.Context) error {
 		HealthCheckRetries:           c.Int64("healthcheck-retries"),
 		HealthCheckStartPeriod:       c.Int64("healthcheck-start-period"),
 		HealthCheckTimeout:           c.Int64("healthcheck-timeout"),
+		ServiceNetworkAssignPublicIp: c.StringSlice("service-network-assign-public-ip"),
 		ServiceNetworkSecurityGroups: c.StringSlice("service-network-security-groups"),
 		ServiceNetworkSubnets:        c.StringSlice("service-network-subnets"),
 	}
